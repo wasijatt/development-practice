adc02e692695713c4d324c81d0cd02ebc9694d17
MUHAMMAD <muhmmadwasim5686@gmail.com>
2024-01-26 06:10:45 -0800

    more changes in home

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div className='bg-black'>
+      <h1 className='text-whit'>testing </h1>
+      <h1 className='text-whit'>testing </h1>
+    </div>
+  )
+}
