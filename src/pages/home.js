4b443dd3d05bca02f9e18d265ea041d4d49b488e
MUHAMMAD <muhmmadwasim5686@gmail.com>
2024-01-26 08:42:22 -0800

    home js additoin

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
-export default function home() {
+import homeCss from 'src/styles/home.module.css'
+ function home() {
   return (
-    <div className='bg-black'>
-      <h1 className='text-whit'>testing </h1>
-      <h1 className='text-whit'>testing </h1>
+    <div className={homeCss.div}>
+      <h1 className={homeCss.h1}>testing </h1>
+      <h1 className={homeCss.h1}>testing </h1>
     </div>
   )
 }
+export default home;
